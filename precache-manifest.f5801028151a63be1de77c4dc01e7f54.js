10c5c794ed3d97e3783fe452ecdbb75818d3cc63
devgulati1 <gulatidev84@gmail.com>
2020-07-08 08:10:11 +0530

    Updates

---
+++ precache-manifest.f5801028151a63be1de77c4dc01e7f54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e68ead23b051cde3a9ce45b471b70212",
+    "url": "/WeatherCast/index.html"
+  },
+  {
+    "revision": "466b602985475ab35271",
+    "url": "/WeatherCast/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c9e720c645fa4c362815",
+    "url": "/WeatherCast/static/css/main.25b42bef.chunk.css"
+  },
+  {
+    "revision": "466b602985475ab35271",
+    "url": "/WeatherCast/static/js/2.6520a447.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherCast/static/js/2.6520a447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e720c645fa4c362815",
+    "url": "/WeatherCast/static/js/main.a3b378a0.chunk.js"
+  },
+  {
+    "revision": "a6fcd18eeeae313e03c1",
+    "url": "/WeatherCast/static/js/runtime-main.97d3b3a8.js"
+  }
+]);
